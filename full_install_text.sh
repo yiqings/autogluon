e96441ab08fc9a7c2e1b5bdbb84094c433af0181
Yiqing Shen <45586603+yiqings@users.noreply.github.com>
2022-02-22 10:28:54 +0800

    Update full_install_text.sh

--- full_install_text.sh
+++ full_install_text.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
-python3 -m pip install  common/[tests]
-python3 -m pip install  core/[all,tests]
-python3 -m pip install  features/
-python3 -m pip install  text/
-python3 -m pip install  autogluon/
+python3 -m pip install -e common/[tests]
+python3 -m pip install -e core/[all,tests]
+python3 -m pip install -e features/
+python3 -m pip install -e text/
+python3 -m pip install -e autogluon/
